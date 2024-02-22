4acc596514142e5cbd884d377739bc3ed35821e9
Abhijit Rabha <88610322+codewithrabha@users.noreply.github.com>
2024-02-22 16:25:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const checkInInput = document.getElementById('check_in');
+const checkInBtn = document.getElementById('check_in_btn')
 const checkOutInput = document.getElementById('check_out');
+const checkOutBtn = document.getElementById('check_out_btn')
+
+
+checkInBtn.addEventListener("click", () => {
+    try {
+        checkInInput.showPicker();
+    } catch (error) {
+      // Fall back to another picker mechanism
+    }
+  });
+
+  checkOutBtn.addEventListener("click", () => {
+    try {
+        checkOutInput.showPicker();
+    } catch (error) {
+      // Fall back to another picker mechanism
+    }
+  });
+
 
 function updateCheckOutMinDate() {
     const checkInDate = new Date(checkInInput.value);
