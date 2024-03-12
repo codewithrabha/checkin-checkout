5693b8c4929c1f9a75baf566b304f920b00f4983
Abhijit Rabha <mr.abhijitrabha@gmail.com>
2024-03-12 19:14:24 +0530

    updated

--- index.js
+++ index.js
@@ -4,42 +4,39 @@ const checkOutInput = document.getElementById('check_out');
 const checkOutBtn = document.getElementById('check_out_btn')
 
 
-checkInBtn.addEventListener("click", () => {
-    try {
-        checkInInput.showPicker();
-    } catch (error) {
-      // Fall back to another picker mechanism
-    }
-  });
+const showPickerTrigger = (input) => {
+  try {
+    input.showPicker();
+  } catch (error) {
+    // Fall back to another picker mechanism
+  }
+}
 
-  checkOutBtn.addEventListener("click", () => {
-    try {
-        checkOutInput.showPicker();
-    } catch (error) {
-      // Fall back to another picker mechanism
-    }
-  });
+const removeDesabled = () => {
+    checkOutInput.removeAttribute('disabled')
+    checkOutBtn.removeAttribute('disabled')
+}
 
 
 function updateCheckOutMinDate() {
-    const checkInDate = new Date(checkInInput.value);
-    checkOutInput.min = formatDate(new Date(checkInDate.setDate(checkInDate.getDate() + 1)));
-    checkOutInput.disabled = false;
+  const checkInDate = new Date(checkInInput.value);
+  checkOutInput.min = formatDate(new Date(checkInDate.setDate(checkInDate.getDate() + 1)));
+  checkOutInput.disabled = false;
 }
 
 function formatDate(date) {
-    const yyyy = date.getFullYear();
-    const mm = String(date.getMonth() + 1).padStart(2, '0');
-    const dd = String(date.getDate()).padStart(2, '0');
-    return `${yyyy}-${mm}-${dd}`;
+  const yyyy = date.getFullYear();
+  const mm = String(date.getMonth() + 1).padStart(2, '0');
+  const dd = String(date.getDate()).padStart(2, '0');
+  return `${yyyy}-${mm}-${dd}`;
 }
 
 checkInInput.addEventListener('input', () => {
-    updateCheckOutMinDate();
+  updateCheckOutMinDate();
 });
 
 checkOutInput.addEventListener('input', () => {
-    checkInInput.max = checkOutInput.value;
+  checkInInput.max = checkOutInput.value;
 });
 
 checkInInput.min = formatDate(new Date());
